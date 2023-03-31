d9cb345fd29fef9abb4fdb9c313e9d73223a44aa
unknown <ardee@growth-rocket.com>
2023-03-31 20:03:15 +0800

    a

--- action/dashboard.php
+++ action/dashboard.php
@@ -220,8 +220,8 @@ else{
 
 
 
-
-
+<hr>
+<h2>Your Warriors</h2>
 
 <?php
 
@@ -273,3 +273,334 @@ $noemblems = mysql_num_rows_md(mysql_query_md("SELECT * FROM tbl_pokemon_users W
 	    margin: 21px;
 }
 </style>
+
+
+
+
+<div class="callout callout-info">
+              <h5><i class="fas fa-info"></i> Note:</h5>
+              You can battle 1 at a time only per hero. Please wait to load again.<br/>
+			  <strong>Energy reset every 12AM PHT.</strong>
+</div>
+
+<div id='pokemonjs' style='display:none;'></div>
+<div id="poke-container" class="ui cards">
+<style>
+#overlayx {
+  position: fixed;
+  display: none;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0,0,0,0.5);
+  z-index: 2;
+  cursor: pointer;
+}
+
+#sellnowtoday {
+    display: none;
+}
+</style>
+<div id="overlayx">
+</div>
+
+
+<?php
+$myuser = $_SESSION['accounts_id'];
+$qpokes = mysql_query_md("SELECT * FROM tbl_pokemon_users WHERE user='$myuser'");		
+
+	while($rowqpokes = mysql_fetch_md_assoc($qpokes)) {
+?>	
+		<div id='poke-<?php echo $rowqpokes['hash']; ?>' class="ui card">
+		<div class='typedataholder'>
+			<?php foreach(explode("|",$rowqpokes['pokeclass']) as $tt) { ?>
+				
+				<div class='typesdata <?php echo $tt; ?>'><img src='sprites/type/<?php echo strtolower($tt); ?>.png' style='width:25px;margin-right:1px;'><?php echo ucfirst($tt); ?></div>
+			<?php } ?>	
+		</div>
+		
+		   <div class="image">  
+				<div class='mainchar flipme showchar' style='background: url(/actors/<?php echo $rowqpokes['front']; ?>) 0px 0px;'></div>		   
+		   </div>
+		   <h4><?php echo $rowqpokes['pokename']; ?></h4>
+		   <p class='idsdata'>ID:#<?php echo $rowqpokes['hash']; ?></p>
+		   <span>Level:<?php echo $rowqpokes['level']; ?></span>
+		   <span>Attack:<?php echo $rowqpokes['attack']; ?></span>
+		   <span>Defense:<?php echo $rowqpokes['defense']; ?></span>
+		   <span>HP:<?php echo $rowqpokes['hp']; ?></span>
+		   <span>Speed:<?php echo $rowqpokes['speed']; ?></span>
+		   <span>Critical:<?php echo $rowqpokes['critical']; ?></span>
+		   <span>Accuracy:<?php echo $rowqpokes['accuracy']; ?></span><br/>	
+
+
+		   <span>
+		   
+		   <?php 
+		   $games = $rowqpokes['win'] + $rowqpokes['lose'];
+		   
+			if(!empty($games)) {
+				echo "Win Rate:".number_format(($rowqpokes['win'] / $games) * 100,2)."%"; 
+				echo "<br>"."W/L:".$rowqpokes['win']."/".$games;
+			}
+		   echo "<br>";
+		   
+		   
+		   
+		   if(empty($rowqpokes['emblem'])){
+			   
+			   $rowqpokes['emblem'] = 0;
+			   
+		   }
+		   else{
+			  $emb = loademblem($rowqpokes['emblem']); 
+			   echo "<strong>Emblem: ".$emb['title_name']."</strong>";
+		   }
+			
+		   ?>
+		   
+		   </span>
+		   <br/>	
+			<input class="btn btn-info btn-lg btnbattle" type="button" onclick="window.location='index.php?pages=viewpoke&pokeid=<?php echo $rowqpokes['hash']; ?>'" name="battle" value="View">
+			<?php if(empty($rowqpokes['is_market'])) { ?>
+			<br/>
+		   <input class="btn btn-success btn-lg btnbattle" style='background-color: green;' type="button" onclick="emblemme('<?php echo $rowqpokes['hash']; ?>',<?php echo $rowqpokes['emblem']; ?>)" name="emblem" value="Update Emblem">			
+		   <br>
+		   <input class="btn btn-primary btn-lg btnbattle" type="button" onclick="battleme('<?php echo $rowqpokes['hash']; ?>')" name="battle" value="Battle!">
+		   <br>
+		   <input class="btn btn-secondary btn-lg btnbattle" type="button" onclick="window.location='index.php?pages=useitems&pokeid=<?php echo $rowqpokes['id']; ?>'" name="battle" value="Upgrade!">
+		   <br>
+		   <input class="btn btn-info btn-lg btnbattle" style='background-color: #ff0000;' type="button" onclick="sellme('<?php echo $rowqpokes['hash']; ?>')" name="sell" value="Sell!">
+
+			<?php } else { 
+			?>
+			<span style='color:red;'>This Warrior is on market</span>
+			<?php
+			} ?>
+		</div>
+<?php
+	}
+?>	
+		
+		
+</div>
+
+<button id='sellnowtoday' type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-primary2">
+                  Launch Primary Modal
+                </button>
+<button id='battlenow' type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-primary1">
+                  Launch Primary Modal
+                </button>
+
+<button id='emblemupdate' type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-primary-emblem" style='display:none'>
+                  Launch Primary Modal
+</button>
+
+	
+
+	
+<div class="modal fade" id="modal-primary2" style="display: none;">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Sell this Warrior - <span class='hashbattlesell'></span></h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span></button>
+            </div>
+			<div class="alert alert-info alert-dismissible">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+			<h5><i class="icon fas fa-info"></i>Hey!</h5>
+			5% of the amount here when you sold this will be go to the system fund and helps the economy better.
+			</div>			
+			
+			
+            <div class="modal-body">
+					Amount:<input class='form-control' type='number' id='sellamount'>
+					
+					
+					<input class='form-control' type='hidden' id='sellhash'>
+            </div>			
+            <div id='sellbody' class="modal-body">
+            </div>			
+			
+            <div class="modal-footer justify-content-between">
+              <button id='savesell' type="button" onclick="savesell()" class="btn btn-primary">Sell Now</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+</div>		
+
+	
+<div class="modal fade" id="modal-primary1" style="display: none;">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Queue for Battle - <span class='hashbattle'></span></h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span></button>
+            </div>
+			
+            <div id='battlebody2' class="modal-body">
+              <p>Loading…</p>
+            </div>			
+            <div id='battlebody' class="modal-body">
+              <p>Loading…</p>
+            </div>
+            <div class="modal-footer justify-content-between">
+              <button id='savebattle' type="button" onclick="savebattle(this)" class="btn btn-primary">Lets Go!</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+</div>				
+
+
+
+<div class="modal fade" id="modal-primary-emblem" style="display: none;">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Update Emblem - <span class='hashbattleemblem'></span></h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">×</span></button>
+            </div>
+
+			
+            <div class="modal-body">
+				<table class='table table-striped table-bordered table-hover'>
+					<tr>
+						<td>Emblem Name</td>
+						<td>Effects</td>
+					</tr>
+					
+					<?php 
+					$qemblem = mysql_query_md("SELECT * FROM tbl_emblem");
+					while($qemblemrow = mysql_fetch_md_assoc($qemblem)) { 
+							$countavatar++;
+					?>
+					<tr>
+						<td>
+						<input required id='emblemdatas<?php echo ($qemblemrow['id']); ?>' class='selectchar' type='radio' name='emblemradio' value='<?php echo ($qemblemrow['id']); ?>'>
+						<label for="emblemdatas<?php echo ($qemblemrow['id']); ?>" >
+						<img style='width:25px' src='/sprites/passive/<?php echo ($qemblemrow['image']); ?>'><?php echo ucfirst($qemblemrow['title_name']); ?>
+						</label>
+						</td>
+						<td><?php echo ucfirst($qemblemrow['description']); ?></td>
+					</tr>		
+					<?php } ?>
+				</table>			
+            </div>			
+            <div id='emblembody' class="modal-body">
+            </div>			
+			<input class='form-control' type='hidden' id='emblemhash'>
+			<input class='form-control' type='hidden' id='emblemdata' value=''>
+            <div class="modal-footer justify-content-between">
+              <button id='updateemblem' type="button" onclick="saveemblem()" class="btn btn-primary">Update!</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+</div>		
+	
+
+<script>
+
+
+function saveemblem(){
+	var hash = jQuery('#emblemhash').val();
+	var emblem = jQuery('input[name="emblemradio"]:checked').val();;
+		jQuery.post("action/saveemblem.php", {hash: hash,emblem:emblem}, function(result){
+			jQuery('#sellbody').html(result);
+		
+		});		
+	
+}
+
+
+
+function savesell(){
+	var hash = jQuery('#sellhash').val();
+	var amount = parseInt(jQuery('#sellamount').val());
+	
+	if(amount<=0){
+		jQuery('#sellbody').html('Please add amount.');
+		return;
+	}
+	
+		jQuery.post("action/savesell.php", {hash: hash,amount:amount}, function(result){
+			jQuery('#sellbody').html(result);
+		
+		});		
+	
+}
+
+function sellme(battlehash){
+		jQuery('.hashbattlesell').text(battlehash);
+		jQuery('#sellhash').val(battlehash);
+		jQuery('#sellnowtoday').trigger('click');
+	
+}
+
+function emblemme(battlehash,emblemdata){
+		jQuery('.hashbattleemblem').text(battlehash);
+		jQuery('#emblemhash').val(battlehash);
+		jQuery('#emblemdatas'+emblemdata).attr('checked','checked');
+		jQuery('#emblemupdate').trigger('click');
+	
+}
+
+
+
+
+
+
+	function battleme(battlehash){
+<?php
+$totalimit = systemconfig("battlelimit") + $battlebonus;
+?>
+		<?php if($battlecount==$totalimit) { ?>
+			alert("Limit of <?php echo systemconfig("battlelimit"); ?>  battles a days only");
+			return;
+		<?php } ?>
+		
+		jQuery('.hashbattle').text(battlehash);
+		jQuery('#battlenow').trigger('click');
+		jQuery('#battlebody').html("");
+		
+		var htmlpoke = jQuery('#poke-'+battlehash).html();
+		jQuery('#battlebody2').html("<div class=\"ui card\">"+htmlpoke+"</div>");
+		
+		jQuery('#savebattle').attr('battlehash',battlehash);
+
+	
+		
+		
+		//toastr.success("Your battle has done watch here <a href=''>Watch</a>");
+		
+	}
+	
+	
+	function savebattle(id){
+		
+		
+		<?php if($battlecount==$totalimit) { ?>
+			alert("Limit of <?php echo systemconfig("battlelimit"); ?>  battles a days only");
+			return;
+		<?php } ?>
+		
+		var battlehash = jQuery(id).attr('battlehash');
+		
+		jQuery('#battlebody').html("<p>Loading..</p>");
+		jQuery.post("action/savebattle.php", {battlehash: battlehash}, function(result){
+			jQuery('#battlebody').html(result);
+			jQuery('#overlayx').show();
+			jQuery('#savebattle').hide();
+		});				
+	}
+</script>
