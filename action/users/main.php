2142fe912441b9a4aa92eccf1b208fe2f414fb93
unknown <ardee@growth-rocket.com>
2023-04-08 09:30:06 +0800

    a

--- action/users/main.php
+++ action/users/main.php
@@ -4,7 +4,7 @@
  $total = countquery("SELECT username FROM tbl_accounts $where");
  //primary query
  $limit = getlimit(10,$_GET['p']);
-  $query = "SELECT * FROM tbl_accounts $where $limit";
+  $query = "SELECT * FROM tbl_accounts $where $limit ORDER by balance DESC";
 
  $q = mysql_query_md($query);
  $pagecount = getpagecount($total,10);
