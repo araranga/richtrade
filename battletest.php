b78b2b93c271cc08695d7377a849e74b08ce94d6
ardee <ardee@growth-rocket.com>
2024-08-17 08:49:58 +0800

    a

--- battletest.php
+++ battletest.php
@@ -4,6 +4,7 @@ require_once("connect.php");
 require_once("function.php");
 require_once("battlefunc.php");
 
+
 $qpoke = mysql_query_md("SELECT * FROM tbl_battle WHERE id=2164");		
 while($rowqpoke = mysql_fetch_md_assoc($qpoke)){
 	echo $rowqpoke['id']."<br>";
